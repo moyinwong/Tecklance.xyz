a2471000ce7326be2c2ee79c834066c5384e783b
Stanley Lau <siuclau4@gmail.com>
2020-07-15 15:44:23 +0800

    edited task main function

--- public/js/task.js
+++ public/js/task.js
@@ -1,23 +1,24 @@
 //load info of particular task from database
 async function main() {
-    let urlParams = new URLSearchParams(window.location.search);
-    let id = urlParams.get('id')
-    let res = await fetch(`/task/${id}`, {
-        method: 'GET'
-    })
-    let task = await res.json();
-    console.log(task)
-    let titleContainer = document.querySelector("#title");
-    let contentContainer = document.querySelector("#content")
-    
+  let urlParams = new URLSearchParams(window.location.search);
+  let id = urlParams.get("id");
+  let res = await fetch(`/task/${id}`, {
+    method: "GET",
+  });
+  let task = await res.json();
+  console.log(task);
+  document.title = task.title;
+  let titleContainer = document.querySelector("#title");
+  let contentContainer = document.querySelector("#content");
+  let categoryContainer = document.querySelector("#info");
 
-        titleContainer.innerHTML = "";
-        contentContainer.innerHTML
+  titleContainer.innerHTML = "";
+  contentContainer.innerHTML = "";
+  categoryContainer.innerHTML = "";
 
-        titleContainer.innerHTML += task.title;
-        contentContainer.innerHTML += task.content;
-
-       
+  titleContainer.innerHTML += task.title;
+  contentContainer.innerHTML += task.content;
+  categoryContainer.innerHTML += task.category;
 }
 
 main();
