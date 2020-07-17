7e31a16742e383c7f7bb3f21070ff23d77170dd9
Stanley Lau <siuclau4@gmail.com>
2020-07-17 13:55:25 +0800

    Updated Stripe payment with response

--- paymentRoutes.ts
+++ paymentRoutes.ts
@@ -5,15 +5,31 @@ export const paymentRoutes = express.Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 export const charge = (token: string, amount: number) => {
-  return stripe.charges.create({
-    amount: amount,
-    currency: "hkd",
-    source: token,
-  });
+  return stripe.charges.create(
+    {
+      amount: amount,
+      currency: "hkd",
+      source: token,
+    },
+    function (err: any, charge: any) {
+      if (err && err.type === "StripeCardError") {
+        console.log("card is declined");
+      }
+    }
+  );
 };
 
 paymentRoutes.post("/charge", async (req, res) => {
-  console.log(req.body);
-  let data = await charge(req.body.stripeToken, req.body.chargeAmount);
-  console.log(data);
+  try {
+    const data: any = await stripe.charges.create({
+      source: req.body.stripeToken,
+      amount: req.body.chargeAmount * 100,
+      currency: "hkd",
+    });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode).send(err.decline_code);
+  }
+  return res.send("Payment was successful");
 });
--- public/js/payment.js
+++ public/js/payment.js
@@ -1,1 +1,8 @@
+// const Stripe = require("stripe");
+// const stripe = Stripe(
+//   "pk_test_51H5XpOHTdw6tu9ehsd9bMptA9hvCutIylWtuAxXDSGfSvWogRdEnWHdWBc8cacItTR8TsfamlvMeUXX3k2lNEtkf00dIJl0dvT"
+// );
 
+// const stripeElement = stripe.elements();
+
+// console.log(stripe);
