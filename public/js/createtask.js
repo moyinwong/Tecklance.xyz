39ed518ae88d98adb4a1253c00141fd6e476f92c
Stanley Lau <siuclau4@gmail.com>
2020-07-21 22:30:01 +0800

    Update create task function

--- public/js/createtask.js
+++ public/js/createtask.js
@@ -1,12 +1,12 @@
 //login button
-document.querySelector('.login-button').onclick = () => {
-  location.href = "/login.html"
-}
+document.querySelector(".login-button").onclick = () => {
+  location.href = "/login.html";
+};
 
 //home logo button
-document.querySelector('.logo-button').onclick = () => {
-  location.href = "/"
-}
+document.querySelector(".logo-button").onclick = () => {
+  location.href = "/";
+};
 
 //sidebar function
 function openNav() {
@@ -21,20 +21,19 @@ function closeNav() {
 async function checkLogin() {
   let res = await fetch("/current-user");
   let user = await res.json();
-  
+
   if (res.status == 200 && user) {
     if (user.image_user) {
       document.querySelector(".login-button-container").innerHTML = `
     <div><button onclick="openNav()" class="user-profile-button" type="button"><img class="img-fluid" src="/uploads/${user.image_user}"></button></div>
     <div>${user.username}</div>
     <button class="login-button" type="button" onclick="location.href='/logout'">LOG OUT</button>`;
-    } 
-    
-    else {
+    } else {
       document.querySelector(".login-button-container").innerHTML = `
       <div><button onclick="openNav()" class="user-profile-button" type="button"><i class="far fa-user"></i></button></div>
       <div>${user.username}</div>
       <button class="login-button" type="button" onclick="location.href='/logout'">LOG OUT</button>`;
+      document.querySelector("#remain_amt").innerHTML = user.remain_amt;
     }
   }
 }
@@ -45,30 +44,53 @@ async function createTask(event) {
 
     const form = event.target;
 
+    //get user information
+    let userRes = await fetch("/current-user");
+    let user = await userRes.json();
+
     const formData = new FormData();
 
     formData.append("title", form.title_input.value);
     formData.append("content", form.content_input.value);
     formData.append("category", form.category.value);
-    
+    formData.append("offered_amt", parseInt(form.offered_amt.value));
+    formData.append("remain_amt", parseInt(user.remain_amt));
+
     //append image in the last step
     if (form.image) {
       formData.append("image", form.image.files[0]);
     }
 
-    let userRes = await fetch("/current-user");
-    let user = await userRes.json();
+    //check if there is enough money
+
+    if (parseInt(form.offered_amt.value) > parseInt(user.remain_amt)) {
+      //add class to show alert
+      document.querySelector("#offered_amt_alert").classList.add("show");
+      setTimeout(() => {
+        document.querySelector("#offered_amt_alert").classList.remove("show");
+      }, 3000);
+      return;
+    }
 
     //app.post
-    let res = await fetch(`/create-task/${user.email}`, {
+    let res = await fetch(`/create-task/${user.id}`, {
       method: "POST",
       body: formData,
     });
-    if (res.success && res.status == 200) {
-      console.log('task created')
-    }
-    if ((res.status = 401)) {
-      console.log(res);
+
+    if (res.status === 201) {
+      document.querySelector("#response").innerHTML = await res.json();
+      document.querySelector("#response").classList.add("show");
+
+      setTimeout(() => {
+        window.location = "/";
+      }, 3000);
+    } else {
+      document.querySelector("#response").innerHTML = await res.json();
+      document.querySelector("#response").classList.add("show");
+      setTimeout(() => {
+        document.querySelector("#response").classList.remove("show");
+      }, 3000);
     }
   } catch (err) {
     console.log(err);
@@ -76,6 +98,5 @@ async function createTask(event) {
 }
 
 document.querySelector("#task-form").addEventListener("submit", createTask);
-
 
 checkLogin();
--- taskRoutes.ts
+++ taskRoutes.ts
@@ -2,13 +2,16 @@ import express from "express";
 import { Task } from "./models";
 import { client, upload } from "./main";
 import { Usertask } from "./models";
+import { logger } from "./logger";
 
 export const taskRoutes = express.Router();
 
 taskRoutes.get("/create-task/:user", async (req, res) => {
-  let creator_id = (req.params.user).toString();
-  let result = await client.query(/*sql*/ `SELECT * FROM task where creator_id = $1`,
-  [creator_id]);
+  let creator_id = req.params.user.toString();
+  let result = await client.query(
+    /*sql*/ `SELECT * FROM task where creator_id = $1`,
+    [creator_id]
+  );
   let tasks: Task[] = result.rows;
   res.json(tasks);
   // console.log(tasks)
@@ -44,31 +47,41 @@ taskRoutes.get("/usertask/:userId", async function (req, res) {
 });
 
 //create task
-taskRoutes.post("/create-task/:user", upload.single("image"), async (req, res) => {
-  let email = (req.params.user).toString();
-  try {
-    const { title, content, category } = req.body;
+taskRoutes.post(
+  "/create-task/:userId",
+  upload.single("image"),
+  async (req, res) => {
+    let user_id = req.params.userId.toString();
+    try {
+      const { title, content, category, offered_amt, remain_amt } = req.body;
 
-    let image: string | "";
-    if (req.file) {
-      image = req.file.filename;
-    } else {
-      image = "";
+      let image: string | "";
+      if (req.file) {
+        image = req.file.filename;
+      } else {
+        image = "";
+      }
+      //update remaining amount
+      await client.query(
+        /*sql*/ `UPDATE users SET remain_amt = $1 WHERE id=$2`,
+        [remain_amt - offered_amt, user_id]
+      );
+
+      //create task in SQL
+      await client.query(
+        /*sql*/ `INSERT INTO task (title, category, content, image_task, creator_id, offered_amt) VALUES
+    ($1,$2,$3,$4,$5, $6)`,
+        [title, category, content, image, user_id, offered_amt]
+      );
+
+      return res
+        .status(201)
+        .json(
+          "Task is successfully created. Will redirect to homepage automatically"
+        );
+    } catch (err) {
+      logger.error(err.toString());
+      return res.status(401).json(err);
     }
-    await client.query(
-      /*sql*/ `INSERT INTO task (title, content, category, image_task, creator_id) VALUES
-    ($1,$2,$3,$4, (SELECT id from users where email = $5 LIMIT 1))`,
-      [title, content, category, image, email]
-    );
-
-    //
-    //
-    //
-    //Add response after created
-    //
-    //
-    res.status(200).json({ success: true });
-  } catch (err) {
-    res.status(401);
   }
-});
+);
