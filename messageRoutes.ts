77eb1468c613ebaed2aff9374bf83f3411ea0b80
Stanley Lau <siuclau4@gmail.com>
2020-07-24 22:15:56 +0800

    Added reject route

--- messageRoutes.ts
+++ messageRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { client } from "./main";
 import { Message } from "./models";
 import { logger } from "./logger";
+import { isLoggedInAPI } from "./guards";
 
 export const messageRoutes = express.Router();
 
@@ -34,13 +35,16 @@ messageRoutes.get("/getMessage", async (req, res) => {
 messageRoutes.put("/message/read", async (req, res) => {
   try {
     let messageId = req.body.message_id;
-    await client.query(/*sql*/`UPDATE messages SET status = 'read' WHERE id = $1`, [messageId]);
+    await client.query(
+      /*sql*/ `UPDATE messages SET status = 'read' WHERE id = $1`,
+      [messageId]
+    );
 
-    res.status(200).json({success:true})
-  } catch(err) {
+    res.status(200).json({ success: true });
+  } catch (err) {
     console.log(err);
   }
-})
+});
 
 //check for unread messages
 messageRoutes.get("/message/unread", async (req, res) => {
@@ -52,8 +56,11 @@ messageRoutes.get("/message/unread", async (req, res) => {
     };
 
     let userId = await getUserId(req);
-    let result = await client.query(/*sql*/ `SELECT * FROM messages 
-    WHERE recipient_id = $1 AND status IS NULL`, [userId]);
+    let result = await client.query(
+      /*sql*/ `SELECT * FROM messages 
+    WHERE recipient_id = $1 AND status IS NULL`,
+      [userId]
+    );
     let messages = result.rows;
 
     if (messages) {
@@ -61,9 +68,48 @@ messageRoutes.get("/message/unread", async (req, res) => {
     } else {
       return res.status(201);
     }
-
-  } catch(err) {
+  } catch (err) {
     logger.error(err.toString());
-    return res.status(401).json(err)
+    return res.status(401).json(err);
   }
-})
+});
+
+//reject
+messageRoutes.post("/rejectFiles", isLoggedInAPI, async (req, res) => {
+  try {
+    const getTaskId = async (req) => {
+      if (req.header && req.headers.referer) {
+        return req.headers.referer.replace(
+          "http://localhost:8080/task.html?id=",
+          ""
+        );
+      }
+    };
+    const taskId: string = await getTaskId(req);
+
+    //get accepted user id by req.session
+    const getAcceptUserId = async (taskId) => {
+      const accepted_user_id = (
+        await client.query(
+          /*sql*/ `SELECT accepted_user_id FROM task WHERE id = $1`,
+          [taskId]
+        )
+      ).rows[0].accepted_user_id;
+      return accepted_user_id;
+    };
+
+    const accepted_user_id: number = await getAcceptUserId(taskId);
+
+    const content = `The upload files do not meet the requirement. Please upload again after finished.`;
+
+    await client.query(
+      /*sql*/ `INSERT INTO messages (recipient_id, content, created_at) VALUES ($1,$2,NOW())`,
+      [accepted_user_id, content]
+    );
+
+    return res.status(201).json("The reject message is sent to freelancer.");
+  } catch (err) {
+    logger.error(err.toString());
+    return res.status(401).json(err);
+  }
+});
