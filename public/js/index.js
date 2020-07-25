0e366743028f4db5ade546151c27f6c1bbe3ff23
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-07-26 02:33:39 +0800

    minor fix for user profile icon

--- public/js/index.js
+++ public/js/index.js
@@ -282,7 +282,7 @@ async function checkLogin() {
   if (res.status == 200 && user) {
     if (user.image_user) {
       document.querySelector(".login-button-container").innerHTML = `
-    <div><button onclick="openNav()" class="user-profile-button" type="button"><img class="img-fluid" src="/uploads/${user.image_user}"></button></div>
+    <div><button onclick="openNav()" class="user-profile-button" type="button"><img class="user-profile-img img-fluid" src="/uploads/${user.image_user}"></button></div>
     <div>${user.username}</div>
     <button class="login-button" type="button" onclick="location.href='/logout'">LOG OUT</button>`;
     } else {
