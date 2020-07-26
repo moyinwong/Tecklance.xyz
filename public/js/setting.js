fcb3a2537545d0c276f4bd312176f5f6ce3d785a
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-07-26 12:00:55 +0800

    add fontawesone icon back to setting page

--- public/js/setting.js
+++ public/js/setting.js
@@ -1,3 +1,37 @@
+//home logo button
+document.querySelector(".logo-button").onclick = () => {
+  location.href = "/";
+};
+
+//check login
+async function checkLogin() {
+  let res = await fetch("/current-user");
+  let user = await res.json();
+
+  if (res.status == 200 && user) {
+    if (user.image_user) {
+      document.querySelector(".login-button-container").innerHTML = `
+      <div><button onclick="openNav()" class="user-profile-button" type="button"><img class="user-profile-img img-fluid" src="/uploads/${user.image_user}"></button></div>
+      <div>${user.username}</div>
+      <button class="login-button" type="button" onclick="location.href='/logout'">LOG OUT</button>`;
+    } else {
+      document.querySelector(".login-button-container").innerHTML = `
+        <div><button onclick="openNav()" class="user-profile-button" type="button"><i class="far fa-user"></i></button></div>
+        <div>${user.username}</div>
+        <button class="login-button" type="button" onclick="location.href='/logout'">LOG OUT</button>`;
+    }
+  }
+}
+
+//sidebar function
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 //auto fill in data
 async function getFullInfo() {
   const res = await fetch("/getFullInfo");
@@ -63,3 +97,5 @@ async function editUserProfile(event) {
 document
   .querySelector("#user-form")
   .addEventListener("submit", editUserProfile);
+
+checkLogin();
